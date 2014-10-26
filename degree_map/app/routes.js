ea6123cd6540b18fcbe16791980f06d88f18a8d8
Evan Forbes <forbes.evan@gmail.com>
2014-10-25 19:34:04 -0500

    remove profile

--- degree_map/app/routes.js
+++ degree_map/app/routes.js
@@ -23,9 +23,6 @@ module.exports = function(app, passport) {
 		res.render('login.ejs', { message: req.flash('loginMessage') });
 	});
 
-	// process the login form
-	// app.post('/login', do all our passport stuff here);
-
 	// =====================================
 	// SIGNUP ==============================
 	// =====================================
@@ -35,21 +32,6 @@ module.exports = function(app, passport) {
 		// render the page and pass in any flash data if it exists
 		res.render('signup.ejs', { message: req.flash('signupMessage') });
 	});
-
-	// process the signup form
-	// app.post('/signup', do all our passport stuff here);
-
-	// =====================================
-	// PROFILE SECTION =====================
-	// =====================================
-	// we will want this protected so you have to be logged in to visit
-	// we will use route middleware to verify this (the isLoggedIn function)
-	app.get('/profile', isLoggedIn, function(req, res) {
-		res.render('profile.ejs', {
-			user : req.user // get the user out of session and pass to template
-		});
-	});
-
 
     // process the schedule form
     //
@@ -73,14 +55,14 @@ module.exports = function(app, passport) {
 
 
   app.post('/signup', passport.authenticate('local-signup', {
-		successRedirect : '/profile', // redirect to the secure profile section
+		successRedirect : '/', // redirect to the secure profile section
 		failureRedirect : '/signup', // redirect back to the signup page if there is an error
 		failureFlash : true // allow flash messages
 	}));
 
   // process the login form
 	app.post('/login', passport.authenticate('local-login', {
-		successRedirect : '/profile', // redirect to the secure profile section
+		successRedirect : '/', // redirect to the secure profile section
 		failureRedirect : '/login', // redirect back to the signup page if there is an error
 		failureFlash : true // allow flash messages
 	}));
