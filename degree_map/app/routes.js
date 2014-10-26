3fbf4d64e9ff25e0e6c573620237d564ad888e88
Evan Forbes <forbes.evan@gmail.com>
2014-10-25 22:54:30 -0500

    Add setup page

--- degree_map/app/routes.js
+++ degree_map/app/routes.js
@@ -55,7 +55,7 @@ module.exports = function(app, passport) {
 
 
   app.post('/signup', passport.authenticate('local-signup', {
-		successRedirect : '/', // redirect to the secure profile section
+		successRedirect : '/setup', // redirect to the secure profile section
 		failureRedirect : '/signup', // redirect back to the signup page if there is an error
 		failureFlash : true // allow flash messages
 	}));
@@ -66,6 +66,16 @@ module.exports = function(app, passport) {
 		failureRedirect : '/login', // redirect back to the signup page if there is an error
 		failureFlash : true // allow flash messages
 	}));
+
+
+	app.get('/setup', function(req, res){
+			res.render('setup.ejs', {user: req.user, reqCourses: ['test','test2']});
+	});
+
+	app.post('/setup', function(req, res){
+		console.log('setup submit: '+JSON.stringify(req.body));
+			res.redirect('/schedule');
+	});
 };
 
 // route middleware to make sure a user is logged in
